c58322bd3590c372db26585b852bde1b6a8e4153
Travis Huynh <huynhtk51@gmail.com>
2024-11-21 19:14:08 -0800

    Added Homepage, login, and basic css structure

---
+++ frontend/src/pages/AuthPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthPage = () => {
+  return (
+    <div className="auth-page">
+      <h1>Login / Register</h1>
+      <p>xxx</p>
+    </div>
+  );
+};
+
+export default AuthPage;
