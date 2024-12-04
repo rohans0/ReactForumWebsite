79304a8b0ef13bf9b4920e0e38ed7aee2f97c625
Matthew David <mdavid3@uw.edu>
2024-12-03 21:20:39 -0800

    search testing changes, still mostly just works for search by user

--- Search/dbtest.js
+++ Search/dbtest.js
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const app = express(); 
+const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(cors());
@@ -24,38 +24,33 @@ db.connect((err) => {
 
 
 app.get('/user/:username', (req, res) => {
-    const username = req.params.username;
-    console.log('Searching for:', username);
-    const queryUsr = 'SELECT UserID FROM User WHERE Username = ?';
+    const username = req.params.username; 
+    console.log('Searching for:', username); 
+    const queryUsr = 'SELECT UserID FROM User WHERE Username = ?'; 
     db.query(queryUsr, [username], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        //res.send(result);
-        if (result.length === 0) {
-            return res.status(404).send('User not found');
-        }
+        if (err) { 
+            console.error('Error querying user:', err); return res.status(500).send(err); 
+        } 
+        if (result.length === 0) { console.log('User not found'); return res.status(404).send('User not found'); 
 
-        const userID = result[0].UserID;
-        const queryPost = 'SELECT * FROM Post WHERE U_UserID = ?';
-        db.query(queryPost, [userID], (err, p_result) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            res.send(p_result);
+        } 
+        const userID = result[0].UserID; console.log('UserID:', userID);
+        const queryThreads = 'SELECT * FROM Thread WHERE U_UserID = ?';
+        db.query(queryThreads, [userID], (err, t_result) => {
+            if (err) { console.error('Error querying threads:', err); return res.status(500).send(err); } console.log('Threads:', t_result); res.send(t_result);
+
+            app.get('/thread/:Title', (req, res) => {
+                const Title = req.params.Title;
+                console.log('Searching for:', Title);
+                const queryThread = 'SELECT * FROM Thread WHERE Title = ?';
+                db.query(queryThread, [Title], (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.send(result);
+                });
+            });
         });
-    });
-});
-
-app.get('/thread/:Title', (req, res) => {
-    const Title = req.params.Title;
-    console.log('Searching for:', Title);
-    const queryThread = 'SELECT * FROM Thread WHERE Title = ?';
-    db.query(queryThread, [Title], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.send(result);
     });
 });
 
